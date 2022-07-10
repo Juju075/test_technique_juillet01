da4ae60504900e72035cd530518621028009954d
elgato <amzfba.1bestbuy@gmail.com>
2022-07-10 12:24:04 +0200

    boostrap vue commented in main.js fix issue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+/** installation manuelle https://bootstrap-vue.org/docs#vue-cli-3 */
+//import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+createApp(App).use(router).mount("#app");
+
+//app.use() call plugin
+/**
+App.use(BootstrapVue);
+App.use(BootstrapVueIcons);
+ */
